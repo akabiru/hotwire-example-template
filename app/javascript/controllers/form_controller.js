802abd6f0b7afbde64a8b68e07a5e969a2356f59
Kabiru Mwenja <makabby@gmail.com>
2022-02-28 13:19:45 +0300

    feat: Hide the preview when JS is enabled

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["preview"]
 
+  connect() {
+    // hide the preview button when we have Stimulus
+    this.previewTarget.hidden = true
+  }
+
   preview() {
     this.previewTarget.click()
   }
