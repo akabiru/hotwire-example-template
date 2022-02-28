2b46689d2d5e7d64329486eee7e5d254a120358c
Kabiru Mwenja <makabby@gmail.com>
2022-02-28 13:27:13 +0300

    chore: Debounce requests

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
+import debounce from "lodash.debounce";
 
 export default class extends Controller {
   static targets = ["preview"]
+
+  initialize() {
+    // delay preview by 300ms to account for fast typing
+    this.preview = debounce(this.preview.bind(this), 300)
+  }
 
   connect() {
     // hide the preview button when we have Stimulus
